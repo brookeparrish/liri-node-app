ea4163e4be7eadb00c6dc040fa1a073ab5c97ccd
Brooke <parrish.e.brooke@gmail.com>
2019-11-01 20:14:19 -0700

    made final touches

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # liri-node-app
+
+Simple node command line app that searches spotify API for specific songs.
--- liri.js
+++ liri.js
@@ -28,39 +28,38 @@ let spotifyRequest = function (songName) {
             let songs = data.tracks.items;
 
             for (let i = 0; i < songs.length; i++) {
-                console.log(i);
-                console.log("artist(s): " + songs[i].artists.map(retreiveArtistNames));
+                console.log("artist: " + songs[i].artists.map(retreiveArtistNames));
                 console.log("song name: " + songs[i].name);
                 console.log("preview song: " + songs[i].preview_url);
                 console.log("album: " + songs[i].album.name);
+                console.log("---------------------------");
             }
         })
 };
 
-let doWhatItSays = function() {
-    fs.readFile("random.txt", "utf8", function(error, data) {
-        console.log(data);
+// let doWhatItSays = function() {
+//     fs.readFile("random.txt", "utf8", function(error, data) {
+        
+//         let dataArray = data.split(",");
 
-        let dataArray = data.split(",");
+//         if (dataArray.length === 2) {
+//             choose(dataArray[0], dataArray[1]);
+//         } else if (dataArray.length === 1) {
+//             choose(dataArray[0]);
+//         }
+//     });
+// };
 
-        if (dataArray.length === 2) {
-            choose(dataArray[0], dataArray[1]);
-        } else if (dataArray.length === 1) {
-            choose(dataArray[0]);
-        }
-    });
-};
-
-var choose = function (caseData, functionData) {
-    switch (caseData) {
+var choose = function (searchUserCmd, userSong) {
+    switch (searchUserCmd) {
         case "spotify-this-song":
-            spotifyRequest(functionData);
+            spotifyRequest(userSong);
             break;
-        case "do-what-it-says":
-            doWhatItSays();
-            break;
-    default:
-        console.log("LIRI doesn't know that");
+        // case "do-what-it-says":
+        //     doWhatItSays();
+        //     break;
+        default:
+            console.log("LIRI doesn't know that");
     }
 };
 
